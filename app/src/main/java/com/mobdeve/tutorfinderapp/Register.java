4bcaa4c2fe9c770f2ef282399797687e6bfbd5b6
Eugene Perez <eugene_perez@dlsu.edu.ph>
2021-05-16 23:23:12 +0800

    Update register

--- app/src/main/java/com/mobdeve/tutorfinderapp/Register.java
+++ app/src/main/java/com/mobdeve/tutorfinderapp/Register.java
@@ -10,10 +10,26 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+* TODO:
+*  line 91
+*  line 101
+*  amazing
+* */
 
 public class Register extends AppCompatActivity {
 
@@ -23,12 +39,8 @@ public class Register extends AppCompatActivity {
     private EditText text_lastname;
     private EditText text_contact;
 
-    private String username;
-    private String password;
-    private String firstname;
-    private String lastname;
-    private String contact;
     private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +49,22 @@ public class Register extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+
         text_username = findViewById(R.id.regEmailEt);
         text_password = findViewById(R.id.regPasswordEt);
-        //text_firstname = findViewById(R.id.reg);
+        text_lastname = findViewById(R.id.regLastNameEt);
+        text_firstname = findViewById(R.id.regFirstNameEt);
         text_contact = findViewById(R.id.regContactNumEt);
+
+
 
         Intent i = getIntent();
         String type = i.getStringExtra("Type");
 
-        createAccount(username, password, type, firstname, lastname, contact);
+        createAccount(text_username.getText().toString(),
+                text_password.getText().toString(),
+                type, text_firstname.getText().toString(),
+                text_lastname.getText().toString(), text_contact.getText().toString());
     }
 
     public void createAccount( String username, String password, String type,
@@ -58,16 +77,54 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Intent pasokgago = new Intent(Register.this, Homepage.class);
-                            
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("Contact details", contact);
+                            user.put("Email", username);
+                            user.put("First name", firstname);
+                            user.put("Last name", lastname);
+
+                            if(type.equals("Tutor")){
+                                db.collection("Tutors").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Intent pasokgago = new Intent(Register.this, Homepage.class);
+                                        //add Tutor to collection
+                                        startActivity(pasokgago);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w("TAG", "Error adding document", e);
+                                    }
+                                });
+                            }//add Tutee
+
 
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("TAG", "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(Register.this, "", Toast.LENGTH_SHORT).show();.makeText(getApplicationContext(), "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Register.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+    }
+
+    public String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0; i<messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
