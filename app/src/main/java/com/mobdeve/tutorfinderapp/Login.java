061e6360819fa2d79822b4a305800d0156a2d95c
Eugene Perez <eugene_perez@dlsu.edu.ph>
2021-05-16 20:03:17 +0800

    Updating authentication system

--- app/src/main/java/com/mobdeve/tutorfinderapp/Login.java
+++ app/src/main/java/com/mobdeve/tutorfinderapp/Login.java
@@ -2,13 +2,79 @@ package com.mobdeve.tutorfinderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+    private String username;
+    private String password;
+    private EditText text_username;
+    private EditText text_password;
+    private Button loginbtn;
+    private FirebaseAuth mAuth;
+
+
+    private int authLogin(String username, String password){
+        if(password.length() < 8){
+            return 0;
+        }else if(username.equals("admin") && password.equals("password")){
+            Intent adminintent = new Intent(Login.this, AdminPage.class);
+            startActivity(adminintent);
+            return 1;
+        }else if(username.equals("user") && password.equals("password")){
+            Intent userintent = new Intent(Login.this, Homepage.class);
+            startActivity(userintent);
+            return 2;
+        }
+        return 0;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        text_username = findViewById(R.id.loginEmailEt);
+        text_password = findViewById(R.id.loginPasswordEt);
+        loginbtn = findViewById(R.id.loginPageBtn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                username = text_username.getText().toString();
+                password = text_password.getText().toString();
+
+                if(authLogin(username, password) != 0){
+                    Toast toast = Toast.makeText(getApplicationContext(), "successfully logged in", Toast.LENGTH_SHORT);
+                    toast.show();
+                }else{
+                    Toast toast = Toast.makeText(getApplicationContext(), "wrong login creds bro", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //do something
+        }
+    }
+
 }
--- app/src/main/java/com/mobdeve/tutorfinderapp/Register.java
+++ app/src/main/java/com/mobdeve/tutorfinderapp/Register.java
@@ -2,13 +2,32 @@ package com.mobdeve.tutorfinderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+
+
+    public void createAccount(String type, String username, String password){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Intent i = getIntent();
+        String type = i.getStringExtra("Type");
+
+        if(type.equals("tutor")){
+
+        }else if(type.equals("tutee")){
+
+        }
     }
 }
