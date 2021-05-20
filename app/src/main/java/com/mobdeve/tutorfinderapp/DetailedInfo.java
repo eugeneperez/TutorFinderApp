daea8104d2bacf578281893ee7b145cf18cf7435
Eugene Perez <eugene_perez@dlsu.edu.ph>
2021-05-20 19:23:19 +0800

    creating detailed info page

--- app/src/main/java/com/mobdeve/tutorfinderapp/DetailedInfo.java
+++ app/src/main/java/com/mobdeve/tutorfinderapp/DetailedInfo.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.gson.Gson;
 
 public class DetailedInfo extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_info);
+
+        currentUser = mAuth.getCurrentUser();
 
         Gson gson = new Gson();
         Intent i = getIntent();
